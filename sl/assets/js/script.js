b4eec02f273db3af6ae285b87b6100d63149c568
jfdigital82 <104395977+jfdigital82@users.noreply.github.com>
2022-06-01 23:28:43 +0100

    d

--- sl/assets/js/script.js
+++ sl/assets/js/script.js
@@ -79,8 +79,9 @@ function nextPrev(n) {
   // This function will figure out which tab to display
   var x = document.getElementsByClassName("multisteps_form_panel");
   // Exit the function if any field in the current tab is invalid:
-  // if (n == 1 && !validateForm()) return false;
+  if (n == 1 && !validateForm()) return false;
   // Hide the current tab:
+  $(".message").hide();
   x[currentTab].style.display = "none";
   // Increase or decrease the current tab by 1:
   currentTab = currentTab + n;
@@ -96,18 +97,22 @@ function nextPrev(n) {
 
 function validateForm() {
   // This function deals with validation of the form fields
-  var x, y, i, valid = true;
+  var x, y, i, countcheck, valid = false;
+  countcheck = $(".multisteps_form_panel").find("li.active").length;
   x = document.getElementsByClassName("multisteps_form_panel");
   y = x[currentTab].getElementsByTagName("input");
   // A loop that checks every input field in the current tab:
-  for (i = 0; i < y.length; i++) {
-    // If a field is empty...
-    if (y[i].value == "") {
-      // add an "invalid" class to the field:
-      y[i].className += " invalid";
-      // and set the current valid status to false:
-      valid = false;
-    }
+  // for (i = 0; i < y.length; i++) {
+  //   // If a field is empty...
+  //   if (y[i].checked) {
+  //     countcheck= 1;
+  //   } 
+  // }
+  if(countcheck > 0){
+    valid = true;
+    $(".multisteps_form_panel").find("li.active").remove();
+  } else {
+    $(".message").show();
   }
   // If the valid status is true, mark the step as finished and valid:
   if (valid) {
